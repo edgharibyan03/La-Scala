b062a3c5225ade93e5f38113c0e358631c1b0f63
Edgar Gharibyan <edgharibyan1@gmail.com>
2020-07-17 10:47:57 +0400

    v4

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.staging-body__img').on('mouseenter', function() {
-        $('.staging-body__link').css('display', 'block', 'transition', '.4s');
-    })
-    $('.staging-body__img').on('mouseleave', function() {
-        $('.staging-body__link').css('display', 'none');
-    })
-})
