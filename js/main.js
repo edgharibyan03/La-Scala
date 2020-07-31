a40d1ca48cfd054ee2d880922722fddd94ac2cf7
Edgar Gharibyan <edgharibyan1@gmail.com>
2020-07-31 16:02:49 +0400

    v8

--- js/main.js
+++ js/main.js
@@ -7,14 +7,20 @@ const burgerMenu = document.querySelector('.navbar-hamb__burger')
 const stagingBodyImg = document.querySelector('.staging-body__img');
 const stagingBodyImages = document.querySelectorAll('.staging-body__img');
 let valueOfSwiper2 = 4;
-let valueOfSwiper3 = 1.3;
+let valueOfSwiper3 = 1.74;
 let menuOpen = false;
-
-if(windowWidth < 1200) {
-    valueOfSwiper2 = 1.5;
-}
 if(windowWidth > 1200) {
-    valueOfSwiper3 = 4;
+    valueOfSwiper2 = 4
+}else if(windowWidth < 1200 && windowWidth > 992 
+        || windowWidth < 992 && windowWidth > 720) {
+    valueOfSwiper2 = 1.55;
+    valueOfSwiper3 = 1.32;
+}else if(windowWidth < 720 && windowWidth > 540) {
+    valueOfSwiper2 = 1.15;
+    valueOfSwiper3 = 0.96;
+}else if(windowWidth > 320 && windowWidth < 540) {
+    valueOfSwiper2 = 1;
+    valueOfSwiper3 = 0.575;
 }
 
 let mySwiper2 = new Swiper('#swiper-container2', {
@@ -27,6 +33,7 @@ let mySwiper3 = new Swiper('#swiper-container3', {
     watchSlidesVisibility: true,
     slidesPerView: valueOfSwiper3,
 });
+
 burger.addEventListener('click', () => {
     if(!menuOpen) {
         burger.classList.add('open');
@@ -37,4 +44,13 @@ burger.addEventListener('click', () => {
     };
     navbar.classList.toggle('navbar-active');
     navbarBrands.classList.toggle('active')
+})
+
+window.addEventListener('resize', () => {
+    if(windowWidth > 992) {
+        navbar.classList.remove('navbar-active')
+        navbarBrands.classList.remove('active')
+        burger.classList.remove('open')
+        menuOpen = false;
+    }
 })
