782d53c65ca92c3f8b9f9a658d8813b09f9ad8a4
Edgar Gharibyan <edgharibyan1@gmail.com>
2020-08-01 16:42:27 +0400

    v9

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 new WOW().init();
 const windowWidth = document.body.clientWidth;
-const navbar = document.querySelector('.navbar-block__links')
+const navbar = document.querySelector('.navbar-block__links');
+const headerNavbar = document.querySelector('.header-navbar');
 const burger = document.querySelector('.navbar-hamb');
 const navbarBrands = document.querySelector('.navbar-brands');
 const burgerMenu = document.querySelector('.navbar-hamb__burger')
 const stagingBodyImg = document.querySelector('.staging-body__img');
 const stagingBodyImages = document.querySelectorAll('.staging-body__img');
 let valueOfSwiper2 = 4;
-let valueOfSwiper3 = 1.74;
+let valueOfSwiper3 = 2;
 let menuOpen = false;
 if(windowWidth > 1200) {
     valueOfSwiper2 = 4
-}else if(windowWidth < 1200 && windowWidth > 992 
-        || windowWidth < 992 && windowWidth > 720) {
+}else if(windowWidth < 1200 && windowWidth > 720) {
     valueOfSwiper2 = 1.55;
     valueOfSwiper3 = 1.32;
 }else if(windowWidth < 720 && windowWidth > 540) {
@@ -21,6 +21,14 @@ if(windowWidth > 1200) {
 }else if(windowWidth > 320 && windowWidth < 540) {
     valueOfSwiper2 = 1;
     valueOfSwiper3 = 0.575;
+}
+
+if(windowWidth < 992) {
+    headerNavbar.classList.remove('animate__animated')
+    headerNavbar.classList.remove('animate__fadeInDown')
+}else{
+    headerNavbar.classList.remove('animate__animated')
+    headerNavbar.classList.add('animate__fadeInDown')
 }
 
 let mySwiper2 = new Swiper('#swiper-container2', {
@@ -52,5 +60,13 @@ window.addEventListener('resize', () => {
         navbarBrands.classList.remove('active')
         burger.classList.remove('open')
         menuOpen = false;
+    }
+
+    if(windowWidth < 992) {
+        headerNavbar.classList.remove('animate__animated')
+        headerNavbar.classList.remove('animate__fadeInDown')
+    }else{
+        headerNavbar.classList.add('animate__animated')
+        headerNavbar.classList.add('animate__fadeInDown')
     }
 })
